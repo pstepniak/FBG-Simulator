8dee741da58d3b408e918a200aedcc89df14c4f4
Piotr Stepniak <Piotr.Stepniak@assecobs.pl>
2017-11-01 22:48:37 +0100

    Zaczątki symulacji

--- WindowsFormsApplication1/FBGManagement/Grating.cs
+++ WindowsFormsApplication1/FBGManagement/Grating.cs
@@ -8,9 +8,12 @@ namespace WindowsFormsApplication1.FBGManagement
 {
     class Grating
     {
-        private int length;
+        private double length;
         private double period;
-        private double refractiveIndexModulation;
+        private double refractiveIndexModulation; //delta_n
+        private const double neff = 1.44688;
+        private const double lambdaB = 0.0000015312;
+        //okres=lambdaB/(2*pi*neff);% delta n
 
         public Grating(double period, int length, double refractiveIndexModulation)
         {
@@ -22,7 +25,13 @@ namespace WindowsFormsApplication1.FBGManagement
         {
             this.length = length;
             this.period = period;
-            refractiveIndexModulation = 0.0001;
+            refractiveIndexModulation = 0.0004;
+        }
+        public Grating()
+        {
+            length = 0.1; //metra
+            period = lambdaB / (2 * Math.PI * neff);
+            refractiveIndexModulation = 0.0004;
         }
     }
 }
---
+++ WindowsFormsApplication1/FBGManagement/Symulation.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApplication1.FBGManagement
+{
+    class Symulation
+    {
+        public Symulation() {
+
+        }
+        public void Symulate()
+        {
+            double[] x =[168.43, 168.43, 168.43, 168.43, 168.43, 168.43, 168.43, 168.43];
+            int ilosc_lambda = 1000;
+            double s = 1530.75 + (3 / ilosc_lambda);
+            int ilosc_sekcji = x.Length;
+            double t = 1 / ilosc_sekcji;
+        }
+
+    }
+}
