17a4d9b1f66adacda5bac37c3d453794152c5bf2
Piotr Stepniak <Piotr.Stepniak@assecobs.pl>
2018-01-13 12:48:46 +0100

    Poprawa skali wykresu

--- WindowsFormsApplication1/FBGForm.cs
+++ WindowsFormsApplication1/FBGForm.cs
@@ -21,44 +21,44 @@ namespace WindowsFormsApplication1
 
         private void FBGForm_Load(object sender, EventArgs e)
         {
-            chartReflection.Series["Reflection"].Points.AddXY(1300, 1);
-            chartReflection.Series["Reflection"].Points.AddXY(1425, 1);
-            chartReflection.Series["Reflection"].Points.AddXY(1430, 0.98);
-            chartReflection.Series["Reflection"].Points.AddXY(1435, 0.95);
-            chartReflection.Series["Reflection"].Points.AddXY(1440, 0.95);
-            chartReflection.Series["Reflection"].Points.AddXY(1445, 0.96);
-            chartReflection.Series["Reflection"].Points.AddXY(1450, 0.97);
-            chartReflection.Series["Reflection"].Points.AddXY(1455, 0.99);
-            chartReflection.Series["Reflection"].Points.AddXY(1500, 0.99);
-            chartReflection.Series["Reflection"].Points.AddXY(1510, 0.97);
-            chartReflection.Series["Reflection"].Points.AddXY(1520, 0.92);
-            chartReflection.Series["Reflection"].Points.AddXY(1530, 0.83);
-            chartReflection.Series["Reflection"].Points.AddXY(1540, 0.72);
-            chartReflection.Series["Reflection"].Points.AddXY(1550, 0.57);
-            chartReflection.Series["Reflection"].Points.AddXY(1560, 0.48);
-            chartReflection.Series["Reflection"].Points.AddXY(1570, 0.42);
-            chartReflection.Series["Reflection"].Points.AddXY(1580, 0.39);
-            chartReflection.Series["Reflection"].Points.AddXY(1590, 0.37);
-            chartReflection.Series["Reflection"].Points.AddXY(1600, 0.36);
-            chartReflection.Series["Reflection"].Points.AddXY(1610, 0.37);
-            chartReflection.Series["Reflection"].Points.AddXY(1620, 0.4);
-            chartReflection.Series["Reflection"].Points.AddXY(1630, 0.44);
-            chartReflection.Series["Reflection"].Points.AddXY(1640, 0.5);
-            chartReflection.Series["Reflection"].Points.AddXY(1650, 0.6);
-            chartReflection.Series["Reflection"].Points.AddXY(1660, 0.74);
-            chartReflection.Series["Reflection"].Points.AddXY(1670, 0.84);
-            chartReflection.Series["Reflection"].Points.AddXY(1680, 0.92);
-            chartReflection.Series["Reflection"].Points.AddXY(1690, 0.97);
-            chartReflection.Series["Reflection"].Points.AddXY(1700, 1);
-            chartReflection.Series["Reflection"].Points.AddXY(1900, 1);
+            //chartReflection.Series["Reflection"].Points.AddXY(1300, 1);
+            //chartReflection.Series["Reflection"].Points.AddXY(1425, 1);
+            //chartReflection.Series["Reflection"].Points.AddXY(1430, 0.98);
+            //chartReflection.Series["Reflection"].Points.AddXY(1435, 0.95);
+            //chartReflection.Series["Reflection"].Points.AddXY(1440, 0.95);
+            //chartReflection.Series["Reflection"].Points.AddXY(1445, 0.96);
+            //chartReflection.Series["Reflection"].Points.AddXY(1450, 0.97);
+            //chartReflection.Series["Reflection"].Points.AddXY(1455, 0.99);
+            //chartReflection.Series["Reflection"].Points.AddXY(1500, 0.99);
+            //chartReflection.Series["Reflection"].Points.AddXY(1510, 0.97);
+            //chartReflection.Series["Reflection"].Points.AddXY(1520, 0.92);
+            //chartReflection.Series["Reflection"].Points.AddXY(1530, 0.83);
+            //chartReflection.Series["Reflection"].Points.AddXY(1540, 0.72);
+            //chartReflection.Series["Reflection"].Points.AddXY(1550, 0.57);
+            //chartReflection.Series["Reflection"].Points.AddXY(1560, 0.48);
+            //chartReflection.Series["Reflection"].Points.AddXY(1570, 0.42);
+            //chartReflection.Series["Reflection"].Points.AddXY(1580, 0.39);
+            //chartReflection.Series["Reflection"].Points.AddXY(1590, 0.37);
+            //chartReflection.Series["Reflection"].Points.AddXY(1600, 0.36);
+            //chartReflection.Series["Reflection"].Points.AddXY(1610, 0.37);
+            //chartReflection.Series["Reflection"].Points.AddXY(1620, 0.4);
+            //chartReflection.Series["Reflection"].Points.AddXY(1630, 0.44);
+            //chartReflection.Series["Reflection"].Points.AddXY(1640, 0.5);
+            //chartReflection.Series["Reflection"].Points.AddXY(1650, 0.6);
+            //chartReflection.Series["Reflection"].Points.AddXY(1660, 0.74);
+            //chartReflection.Series["Reflection"].Points.AddXY(1670, 0.84);
+            //chartReflection.Series["Reflection"].Points.AddXY(1680, 0.92);
+            //chartReflection.Series["Reflection"].Points.AddXY(1690, 0.97);
+            //chartReflection.Series["Reflection"].Points.AddXY(1700, 1);
+            //chartReflection.Series["Reflection"].Points.AddXY(1900, 1);
 
-            foreach (DataPoint p in chartReflection.Series["Reflection"].Points)
-            {
-                double x = p.YValues.First();
-                Random rnd = new Random();
-                double r = (rnd.Next(1, 10))/10;
-                p.SetValueY(x - r);
-            }
+            //foreach (DataPoint p in chartReflection.Series["Reflection"].Points)
+            //{
+            //    double x = p.YValues.First();
+            //    Random rnd = new Random();
+            //    double r = (rnd.Next(1, 10))/10;
+            //    p.SetValueY(x - r);
+            //}
 
             int countOfProbe = 500;
             //double minimalPeriod = 1530.75;
@@ -69,7 +69,7 @@ namespace WindowsFormsApplication1
             List<double> Ry = symulation.Symulate();
             for (int i = 0; i < countOfProbe; i++)
             {
-                chartTransmission.Series["Transmission"].Points.AddXY(i, Ry.ElementAt(i));
+                chartReflection.Series["Reflection"].Points.AddXY(minimalPeriod+(i+1)*((maximalPeriod-minimalPeriod)/countOfProbe), Ry.ElementAt(i));
             }
         }
 
